c2343dc97db9645f32df922ad21664eed9462725
Christian Jensen <cbejensen@gmail.com>
2017-08-09 23:41:46 -0600

    added react-loadable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import Main from './routes/Main';
-import Compare from './routes/Compare';
 import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+import Loadable from 'react-loadable';
+import Loading from 'components/Loading';
+
+const Main = Loadable({
+  loader: () => import('./routes/Main'),
+  loading: Loading
+});
+const Compare = Loadable({
+  loader: () => import('./routes/Compare'),
+  loading: Loading
+});
 
 export default function App(props) {
   return (

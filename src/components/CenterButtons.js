3ad30046b100ad36d2dec6940c51d3fc866e0095
Christian Jensen <cbejensen@gmail.com>
2017-08-12 17:27:54 -0600

    created custom equals sign

--- src/components/CenterButtons.js
+++ src/components/CenterButtons.js
@@ -24,6 +24,21 @@ export default function CenterButtons(props) {
       border: 'none'
     }
   });
+  const EqualsSign = glamorous.div(
+    {
+      borderWidth: '3px 0',
+      borderStyle: 'solid',
+      borderColor: '#fff'
+    },
+    ({ size = '15px' }) => {
+      const val = parseInt(size, 10); // get numbers
+      const unit = size.replace(/[0-9]/g, ''); // get letters
+      return {
+        width: `${val}${unit || 'px'}`,
+        height: `${parseInt(val / 3, 10)}${unit || 'px'}`
+      };
+    }
+  );
   return (
     <Container>
       <div className="filler first" />
@@ -32,7 +47,7 @@ export default function CenterButtons(props) {
       </CircleButton>
       <div className="filler" />
       <CircleButton size="md" style={{ fontSize: '2em' }}>
-        =
+        <EqualsSign size="25px" />
       </CircleButton>
       <div className="filler" />
       <CircleButton icon="forward" size="sm" />
